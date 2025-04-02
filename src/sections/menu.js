6882ffee11e6bcce683b98b3bc74bd730a31e531
Ofallbrains <ofallbrains>
2025-04-02 13:54:24 +0200

    Modifying menu bar

--- src/sections/menu.js
+++ src/sections/menu.js
@@ -5,23 +5,14 @@ const closeBtn = document.getElementById("close-btn");
 
 
 menuBtn.addEventListener("click", function () {
-    menuList.classList.remove("hidden"); // Show menu
-    menuBtn.classList.add("hidden"); // Hide menu button
-    closeBtn.classList.remove("hidden"); // Show close button
+    menuList.classList.remove("hidden");
+    menuBtn.classList.add("hidden");
+    closeBtn.classList.remove("hidden");
 });
 
 closeBtn.addEventListener("click", function () {
-    menuList.classList.add("hidden"); // Hide menu
-    closeBtn.classList.add("hidden"); // Hide close button
-    menuBtn.classList.remove("hidden"); // Show menu button
+    menuList.classList.add("hidden");
+    closeBtn.classList.add("hidden");
+    menuBtn.classList.remove("hidden");
 });
 });
-
-// menuBtn.addEventListener('click', () => {
-//     menuList.classList.toggle('hidden');
-//     if (menuBtn.getAttribute("name") === "menu-outline") {
-//         menuBtn.setAttribute("name", "close-outline");
-//     } else {
-//         menuBtn.setAttribute("name", "menu-outline");
-//     }
-// });
