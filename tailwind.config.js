d665ad5194c6ecc0b73bc5b2e74a8fcb8a00eeb4
Preacher-Y <ysheja@gmail.com>
2025-04-14 10:17:32 +0200

    last resort

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        beige: '#EFECEA',
+        darkGreen: '#00672E',
+      },
+    },
   },
   plugins: [],
 }
