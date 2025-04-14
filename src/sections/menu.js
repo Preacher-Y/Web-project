3c3fb5b6f063ae0487be99580cecdf9e58af4e10
Preacher-Y <ysheja@gmail.com>
2025-04-14 08:05:37 +0200

    final changes

--- src/sections/menu.js
+++ src/sections/menu.js
@@ -1,29 +1,31 @@
 function initializeMenu() {
-    // Wait for elements to be available
     const checkElements = setInterval(() => {
-        const menuBtn = document.getElementById('menu-btn');
-        const menuList = document.getElementById('menu-list');
-        const closeBtn = document.getElementById('close-btn');
-
-        if (menuBtn && menuList && closeBtn) {
-            // Clear interval once elements are found
-            clearInterval(checkElements);
-
-            // Add event listeners
-            menuBtn.addEventListener("click", function () {
-                menuList.classList.remove("hidden");
-                menuBtn.classList.add("hidden");
-                closeBtn.classList.remove("hidden");
-            });
-
-            closeBtn.addEventListener("click", function () {
-                menuList.classList.add("hidden");
-                closeBtn.classList.add("hidden");
-                menuBtn.classList.remove("hidden");
-            });
+      const menuBtn = document.getElementById('menu-btn');
+      const menuList = document.getElementById('menu-list');
+      const closeBtn = document.getElementById('close-btn');
+  
+      if (menuBtn && menuList && closeBtn) {
+        clearInterval(checkElements);
+  
+        
+        if (!window.location.pathname.endsWith('index.html') && window.location.pathname !== '/') {
+          menuBtn.src = '/src/img/Group 5.svg';
+          closeBtn.src = '/src/img/Group 4.svg';
         }
-    }, 100); // Check every 100ms
-}
-
-// Call initializeMenu immediately if script is loaded dynamically
-initializeMenu();
+  
+        
+        menuBtn.addEventListener("click", function () {
+          menuList.classList.remove("hidden");
+          menuBtn.classList.add("hidden");
+          closeBtn.classList.remove("hidden");
+        });
+  
+        closeBtn.addEventListener("click", function () {
+          menuList.classList.add("hidden");
+          closeBtn.classList.add("hidden");
+          menuBtn.classList.remove("hidden");
+        });
+      }
+    }, 100);
+  }
+  
